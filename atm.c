5a98341676e8f4d4efd361a2ab32a5af8dde7ecd
remi.gilliot98@gmail.com <remi.gilliot98@gmail.com>
2025-01-17 11:22:56 +0800

    deposit() and withdraw()

--- atm.c
+++ atm.c
@@ -35,15 +35,45 @@ void change_pin(struct user* account, char *new_pin)
 }
 
 
+void deposit(struct user* account, unsigned int amount)
+{
+	if (amount < 0)
+	{
+		printf("amount has to be greater than 0.\n");
+		return ;
+	}
+	account->balance = account->balance + amount;
+}
+
+void withdraw(struct user* account, unsigned int amount)
+{
+	if (amount < 0)
+	{
+		printf("amount has to be greater than 0.\n");
+		return ;
+	}
+	if (amount > account->balance)
+	{
+		printf("Sorry, the montant you wish to withdraw is greater than your current amount of money in your account.\n Please select an amount between $1 and $%f.", account->balance);
+		return;
+	}
+	account->balance = account->balance - amount;
+}
 
 int main()
 {
 	struct user remi;
 	remi.id = 1;
 	strcpy(remi.pin, "0000");
-	remi.balance = 142;
-	printf("Remi id: %u \n", remi.id);
-	remi.id = set_id(&remi);
-	printf("Remis's new id : %u \n", remi.id);
+	remi.balance = 0;
+	
+	printf("Remi's current balance : $%f \n", remi.balance);
+	deposit(&remi, 42);
+	printf("Remi's current balance : $%f \n", remi.balance);
+
+	withdraw(&remi, 40);
+	printf("Remi's current balance : $%f \n", remi.balance);
+
+	
 }
 
