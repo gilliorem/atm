d71461acc73beb10472b5fcad7aeb7b29e2b7116
remi.gilliot98@gmail.com <remi.gilliot98@gmail.com>
2025-01-17 10:50:52 +0800

    set_id function

--- atm.c
+++ atm.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
+#include <string.h>
 
-struct account {
-	int id;
-	char name[30];
-	long balance;
+struct user {
+	unsigned int id;
+	char pin[4];
+	float balance;
 };
 
-void displayAccount(struct account acc)
+
+unsigned int set_id(struct user* account)
 {
-	printf("Welcome %s\n", acc.name);
-	printf("You have %lg$ in your account.\n");
-	printf("Press 1 to withdraw.\n Press 2 to deposit.\n");
-	scanf("%d");
+	unsigned int new_id;
+	unsigned int previous_id = account->id;
+	printf("Enter your new id:");
+	new_id = scanf("%u", &account->id);	
+	if (new_id || account->id < 0)
+	{
+		printf("Error : id has to be greater than 0.\n");
+		return previous_id;
+	}
+	new_id = account->id;
+	return new_id;
 }
+
+
+void change_pin(struct user* account, char *new_pin)
+{
+	if (strlen(new_pin) != 4)
+	{
+		printf("Enter a 4 digit pin code\n");
+		return;
+	}
+	strcpy(account->pin, new_pin);
+}
+
+
 
 int main()
 {
-	struct account myAccount = {1, "John Doe", 1042};
-	displayAccount(myAccount);
-	return 0;
+	struct user remi;
+	remi.id = 1;
+	strcpy(remi.pin, "0000");
+	remi.balance = 142;
+	printf("Remi id: %u \n", remi.id);
+	remi.id = set_id(&remi);
+	printf("Remis's new id : %u \n", remi.id);
 }
+
