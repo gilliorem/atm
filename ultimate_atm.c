52acf0fa3d899b91c8a5229fa36b4c81f98b576e
remi.gilliot98@gmail.com <remi.gilliot98@gmail.com>
2025-02-07 17:36:09 +0800

    to do: file management : starting with empty array, populated via file info and update it

--- ultimate_atm.c
+++ ultimate_atm.c
@@ -12,9 +12,18 @@ struct user
 };
 
 struct user users[3];
-
 void display_main_menu();
 struct user* active_user = NULL;
+
+void display_users(struct user users[], int max_users)
+{
+	FILE *file = fopen("users", "r");
+
+	unsigned int i = 0;
+	while (i < max_users && fscanf(file, "%d", &users[i].id == 4))
+		i++;
+}
+
 
 void display_user_menu()
 {
@@ -80,18 +89,6 @@ void display_main_menu()
 
 int main ()
 {
-	for (int i = 0; i < 3; i++)
-		users[i].id = i+1;
-	strcpy (users[0].name, "Remi");
-	strcpy(users[1].name, "Romain");
-	strcpy(users[2].name,"Lisa");
-	users[0].pin = 1234;
-	users[1].pin = 5678;
-	users[2].pin = 9012;
-
-	users[0].balance = 23142;
-	users[1].balance = 10000;
-	users[2].balance = 1000000;
-	display_main_menu();
+	return 0;
 }
 
